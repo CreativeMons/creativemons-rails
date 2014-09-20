b2a057bb145aa37f64f2673a5639a716b5a06f0f
Michaël Hoste <michael.hoste@gmail.com>
2014-09-20 16:57:54 +0200

    routes and empty views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
 
+  resource :comments
+  resource :entries
+
+  get '/welcome' => 'pages#welcome'
+  get '/thanks'  => 'pages#thanks'
+
+  root 'entries#index'
+
+  # Route::get('/entries.json',           [ 'as' => 'entries.indexJson',    'uses' => 'EntryController@indexJson'    ]);
+  # Route::get('/author/entries/{token}', [ 'as' => 'entries.showAsAuthor', 'uses' => 'EntryController@showAsAuthor' ]);
+
+  # Route::get('/voter/entries/{id}',           [ 'as' => 'entries.showAsVoter',  'uses' => 'EntryController@showAsVoter'  ]);
+  # Route::get('/voter/entries/{id}/vote_up',   [ 'as' => 'entries.voteUp',       'uses' => 'EntryController@voteUp'       ]);
+  # Route::get('/voter/entries/{id}/vote_down', [ 'as' => 'entries.voteDown',     'uses' => 'EntryController@voteDown'     ]);
+
 end
